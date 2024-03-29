47a78cea8e7cec456c267d938146ccac364d7545
TongmengHAI <tongmeng016@gmail.com>
2024-03-29 14:37:17 +0700

    midterm as default

---
+++ src/main/java/i4/midterm/gradle/GradleWithPostgreApplication.java
@@ -0,0 +1,13 @@
+package i4.midterm.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleWithPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleWithPostgreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/i4/midterm/gradle/GradleWithPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package i4.midterm.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleWithPostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
