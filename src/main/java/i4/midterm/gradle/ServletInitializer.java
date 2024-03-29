6858364412adb37a21909e6df60c4636eaf9cb6d
TongmengHAI <tongmeng016@gmail.com>
2024-03-29 15:11:28 +0700

    make correct

--- src/main/java/i4/midterm/gradle/ServletInitializer.java
+++ src/main/java/i4/midterm/gradle/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(GradleWithPostgreApplication.class)
+		return application.sources(GradleWithPostgreApplication.class);
 	}
 
 }
